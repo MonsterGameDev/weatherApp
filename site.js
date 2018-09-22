1329ecc30387202ab8fb77d06235a13a0b155f1b
Per Høyer <per@brf.dk>
2018-09-22 13:11:27 +0200

    second line

--- site.js
+++ site.js
@@ -1,3 +1,3 @@
 
-     // $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=7a55c7686a3c50284df41b6b3e6757d7", json =>  console.log(json));
+// $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=7a55c7686a3c50284df41b6b3e6757d7", json =>  console.log(json));
 
